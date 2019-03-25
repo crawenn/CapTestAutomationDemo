0d92a28930ee060aacbd05dd5e80fa8eca611a82
Rácz Tamás <tamas.racz@capgemini.com>
2019-03-25 15:44:18 +0100

    search hax

--- CapTestAutomationDemo/Pages/VehicleData/Index.cshtml.cs
+++ CapTestAutomationDemo/Pages/VehicleData/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CapTestAutomationDemo.Models;
 
@@ -20,8 +21,21 @@ namespace CapTestAutomationDemo.Pages.VehicleData
 
         public IList<VehiclesModel> VehiclesModel { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }        
+        public SelectList CarMake { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Make { get; set; }
+      
         public async Task OnGetAsync()
         {
+            var cars = from m in _context.VehiclesModel
+                       select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                cars = cars.Where(s => s.Make.Contains(SearchString));
+            }
+
             VehiclesModel = await _context.VehiclesModel.ToListAsync();
         }
     }
