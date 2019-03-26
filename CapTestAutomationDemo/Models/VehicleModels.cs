7333ee8a56f7b83cb4d60ae2c406f862d816052f
Tamas Racz <tamas.racz@capgemini.com>
2019-03-26 10:49:45 +0100

    model validations

--- CapTestAutomationDemo/Models/VehicleModels.cs
+++ CapTestAutomationDemo/Models/VehicleModels.cs
@@ -10,18 +10,25 @@ namespace CapTestAutomationDemo.Models
     {
         public int ID { get; set; }
 
-        [DataType(DataType.Date)]        
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy}", ApplyFormatInEditMode = true), Required]
         public DateTime Year { get; set; }
+        
+        [StringLength(20, MinimumLength = 2), Required]
         public string Make { get; set; }
-        [Display(Name = "Model")]
+
+        [Display(Name = "Model"), Required]
         public string CarModel { get; set; }
-        [Display(Name = "Body style")]
+
+        [Display(Name = "Body style"), RegularExpression(@"^((S|s)edan|(H|h)atchback|(C|c)oup(e|é)|(L|l)imo?usine|(C|c)abrio)$"), Required]
         public string BodyStyle { get; set; }
-        [Display(Name = "Fuel type")]
+
+        [Display(Name = "Fuel type"), RegularExpression(@"^((P|p)etrol|(D|d)iesel|(H|h)ybrid|(E|e)lectric|LPG|(B|b)iodiesel)$"), Required]
         public string FuelType { get; set; }
-        [Display(Name = "Engine Size")]
+
+        [Display(Name = "Engine Size"), Required]
         public double EngineSize { get; set; }
-        [Display(Name = "Insurance package")]
+
+        [Display(Name = "Insurance package"), Required]
         public int InsurancePackage { get; set; }
     }
 }
